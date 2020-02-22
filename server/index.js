f443541e219763c69432cbc075e935d79211503d
bigyam <aruan.careers@gmail.com>
2020-02-22 17:24:49 -0500

    db, models, api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var db = require('./database');
+
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
@@ -10,8 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use('/api/task', require('./api/task'));
+app.use('/api/room', require('./api/room'));
+
 app.listen(PORT, () => {
 	console.log(`Server Listening Port ${PORT}...`);
+});
+
+db.query('SELECT NOW()', (err, res) => {
+	if(err.error)
+		return console.log(err.error);
+	console.log(`PostgreSQL connected: ${res[0].now}.`);
 });
 
 module.export = app;
