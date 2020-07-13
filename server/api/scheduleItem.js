11990a4f2839dcbd0220b9bcecfc02c0aacea859
bigyam <aruan.careers@gmail.com>
2020-07-13 11:32:41 -0400

    rewriting post server side

--- server/api/scheduleItem.js
+++ server/api/scheduleItem.js
@@ -22,6 +22,31 @@ router.get('/:itemId', function(req, res) {
 });
 
 router.post('/', function (req, res) {
+	var body = req.body;
+	var list = [];
+	body.forEach(item => {
+		if(item.isNew) {
+			//new room, act accordingly
+		} else {
+			//check values for new scheduleItems on existing rooms
+			for(var i = 0; i < item.value.length; i++){
+				if(!item.value[i].id){
+					//insert new schedule item: values room_id, task_id, yearScope.
+					//check if exists
+				} else {
+					//check for updated values? can come from front end if changed.  IsChanged?
+				}
+			}
+		}
+	});
+
+	ScheduleItem.insert(scope, taskId, roomId, function (err, result) {
+		if (err)
+			return res.json(err);
+		return res.json(result);
+	});
+});
+/**router.post('/', function (req, res) {
 	var scope = req.body.scope;
 	var taskId = req.body.taskId;
 	var roomId = req.body.roomId;
@@ -31,7 +56,7 @@ router.post('/', function (req, res) {
 			return res.json(err);
 		return res.json(result);
 	});
-});
+});**/
 
 router.put('/', function(req, res) {
 	var itemId = req.body.itemId;
