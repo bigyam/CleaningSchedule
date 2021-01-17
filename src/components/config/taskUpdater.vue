f1a60083838ba4ec3507be90d57123d972d56902
bigyam <aruan.careers@gmail.com>
2021-01-17 18:18:41 -0500

    main schedule page

---
+++ src/components/config/taskUpdater.vue
@@ -0,0 +1,122 @@
+<template>
+    <v-row dense>
+        <v-card width="100%" outlined>
+            <v-card-title class="primary white--text">
+                <h5 class="font-weight-regular">{{ getRoomName }}</h5>
+            </v-card-title>
+            <v-card-text>
+                <v-list dense>
+                    <v-list-item 
+                        v-for="(taskItem, i) in taskList"
+                        :key="i"
+                        @click.stop="showDetails(taskItem)">
+                        <v-list-item-content>
+                            <v-list-item-title v-text="getTaskName(taskItem)">
+                            </v-list-item-title>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-checkbox v-model="taskItem.isComplete" @change="toggleComplete(taskItem)"/>
+                        </v-list-item-action>
+                    </v-list-item>                    
+                </v-list>
+            </v-card-text>
+        </v-card>
+        <v-dialog v-model="showDetailsDialog" max-width="900px">
+            <v-card>
+                <v-card-title>
+                    Task Details
+                </v-card-title>
+                <v-card-text>
+                    Room: {{ getRoomName }}<br>
+                    Task: {{ taskDetails != null ? getTaskName(taskDetails) : 'empty object'}}<br>
+                    Last Completed: {{ taskDetails != null ? 
+                                            taskDetails.lastComplete != null ? taskDetails.lastComplete : 'lastComplete is null'
+                                            : 'task Details is null' }}
+                </v-card-text>
+            </v-card>            
+        </v-dialog>
+    </v-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+/**
+ * need to add in post to "complete" a task
+ */
+export default {
+    props: {
+        roomDetails: {
+            type: Object,
+            required: true
+        },
+        yearScope: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            showDetailsDialog: false,
+            taskDetails: null
+        }
+    },
+    computed: {
+        ...mapGetters(['rooms', 'tasks']),
+        getRoomName() {
+            let result = "";
+            if(this.rooms.length != 0){
+                result = this.rooms.find(x => x.id == this.roomDetails.roomId).name;
+            }
+            return result;
+        },
+        getTaskName: function() {
+            var vm = this;
+            return function (item) {
+                let result;
+                if(vm.tasks.length != 0){
+                    result = vm.tasks.find(x => x.id == item.task_id).name;
+                }
+                return result;
+            }
+        },
+        taskList() {
+            return this.roomDetails.value.map((item, idx) => ({
+                index: idx,
+                ...item
+            }))
+        }
+    },
+    methods: {
+        fetchData() {
+            this.$service.config.getRooms().then(resp => {
+                this.rooms = resp.data;
+            });
+            this.$service.config.getTasks().then(resp => {
+                this.tasks = resp.data;
+            });
+        },
+        taskExistInRoom(task) {
+            if(task){
+                for(var i = 0; i < this.roomDetails.value.length; i++){
+                    if(this.roomDetails.value[i].task_id == task.id){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
+        toggleComplete(item) {
+            console.log('item: ' + item.isComplete);
+        },
+        showDetails(item) {
+            this.taskDetails = item;
+            this.showDetailsDialog = true;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-container>
+        <v-toolbar text dense color="primary white--text">
+            <v-toolbar-title>Cleaning Schedule</v-toolbar-title>
+            <v-spacer></v-spacer>        
+        </v-toolbar>
+        <v-card outlined width="100%">
+            <v-card-text>
+                <v-row>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>
+                                <v-chip label regular color="#9ba5e0"><h2>Daily</h2></v-chip>
+                            </v-card-title>
+                            <v-card-text>
+                                <task-list v-for="room in dailyRooms" :key="room.roomId" :roomDetails="room" :yearScope="0"/>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>
+                                <v-chip label regular color="#9ba5e0"><h2>Weekly</h2></v-chip>
+                            </v-card-title>
+                            <v-card-text>
+                                <task-list v-for="room in weeklyRooms" :key="room.roomId" :roomDetails="room" :yearScope="1"/>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>
+                                <v-chip label regular color="#9ba5e0"><h2>Monthly</h2></v-chip>
+                            </v-card-title>
+                            <v-card-text>
+                                <task-list v-for="room in monthlyRooms" :key="room.roomId" :roomDetails="room" :yearScope="2"/>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+import _ from 'lodash';
+import TaskList from '.././components/config/taskUpdater.vue';
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    components: {
+        TaskList
+    },
+    data() {
+        return {
+            dailyRooms: [],
+            weeklyRooms: [],
+            monthlyRooms: [],
+        }
+    },
+    computed: {
+        ...mapGetters(['rooms', 'tasks']),
+    },
+    methods: {
+        ...mapActions(['loadRooms', 'loadTasks']),
+        fetchData() {
+            /**this.$service.config.getRooms().then(resp => {
+                this.rooms = resp.data;
+            });
+            this.$service.config.getTasks().then(resp => {
+                this.tasks = resp.data;
+            });**/
+            this.$service.config.getScheduleItems().then(resp => {
+                this.originalData = _.cloneDeep(resp.data);
+                let daily = resp.data.filter(x => x.yearScope == 0);
+                let weekly = resp.data.filter(x => x.yearScope == 1);
+                let monthly = resp.data.filter(x => x.yearScope == 2);
+                this.dailyRooms = this.groupBy(daily, item => item.room_id);
+                this.weeklyRooms = this.groupBy(weekly, item => item.room_id);
+                this.monthlyRooms = this.groupBy(monthly, item => item.room_id);
+                
+            });
+        },
+        groupBy(list, keyGetter) {
+            const array = [];
+            list.forEach((item) => {
+                const key = keyGetter(item);
+
+                const collection = array.find(x => x.roomId == key);
+                if (!collection) {
+                    array.push({roomId: key, value: [item]});
+                } else {
+                    collection.value.push(item);
+                }
+            });
+            return array;
+        },
+    },
+    created() {
+        this.fetchData();
+        this.loadRooms();
+        this.loadTasks();
+    }
+}
+</script>
